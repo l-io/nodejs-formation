38c54b3173890bc474466b14e6766f07f7135c77
Leonardo Rodrigues <lrm5060@gmail.com>
2022-10-11 20:22:16 -0300

    Yargs parsing

--- udemy-courses/the-complete-nodejs/notes-app/app.js
+++ udemy-courses/the-complete-nodejs/notes-app/app.js
@@ -4,37 +4,49 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
-  .command(
-    "remove",
-    "Remove a note",
-    () => {},
-    (argv) => {
+  .command({
+    command: "add",
+    describe: "Add a note",
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string",
+      },
+      body: {
+        describe: "Note body",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: function (argv) {
+      console.log("Title: " + argv.title);
+      console.log("Body: " + argv.body);
+    },
+  })
+  .command({
+    command: "remove",
+    describe: "Remove a note",
+    builder: {},
+    handler: function (argv) {
       console.log("Removing a new note!");
-    }
-  )
-  .command(
-    "add",
-    "Add a note",
-    () => {},
-    (argv) => {
-      console.log("Adding a new note!");
-    }
-  )
-  .command(
-    "list",
-    "List your notes",
-    () => {},
-    (argv) => {
-      console.log("Listing out all notes!");
-    }
-  )
-  .command(
-    "read",
-    "Read a note",
-    () => {},
-    (argv) => {
-      console.log("Reading a note!");
-    }
-  )
+    },
+  })
+  .command({
+    command: "read",
+    describe: "Read a note",
+    builder: {},
+    handler: function (argv) {
+      console.log("Reading a new note!");
+    },
+  })
+  .command({
+    command: "list",
+    describe: "List a note",
+    builder: {},
+    handler: function (argv) {
+      console.log("Listing a new note!");
+    },
+  })
   .demandCommand(1)
   .parse();
